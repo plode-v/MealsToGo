691d9ff39b93cb49f91965871777bd154561a724
Plode <84858705+plode-v@users.noreply.github.com>
2024-03-20 17:32:14 +0700

    yoga flex lesson

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.mainContainer}>
+          <Text>list</Text>
+        </View>
+        <StatusBar style='auto' />
+      </SafeAreaView>
+    </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" && 20,
   },
+  searchContainer: {
+    backgroundColor: 'green',
+    padding: 16,
+  },
+  mainContainer: {
+    backgroundColor: "navy",
+    borderColor: 'black',
+    flex: 1,
+    padding: 16,
+    borderTopColor: 'black',
+    borderTopWidth: 2
+  }
 });
