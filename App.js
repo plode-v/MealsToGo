2b010b4625fb89489a6ab8b7dc72b461dc97c46b
Plode <84858705+plode-v@users.noreply.github.com>
2024-03-21 15:00:18 +0700

    setup restaurants screen

--- App.js
+++ App.js
@@ -1,46 +1,10 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { Searchbar } from "react-native-paper";
-import React, { useState } from "react";
+import React from "react";
+import RestaurantsScreen from "./src/components/features/restaurants/components/restaurant-info.component";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.searchContainer}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={setSearchQuery}
-            value={searchQuery}
-            mode="view"
-          />
-        </View>
-        <View style={styles.mainContainer}>
-          <Text>list</Text>
-        </View>
-        <ExpoStatusBar style="auto" />
-      </SafeAreaView>
+      <RestaurantsScreen />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight, // only applies to Android. Same as SafeAreaView but for Android
-  },
-  searchContainer: {
-    backgroundColor: "green",
-    padding: 16,
-  },
-  mainContainer: {
-    backgroundColor: "darkblue",
-    borderColor: "black",
-    flex: 1,
-    padding: 16,
-    borderTopColor: "black",
-    borderTopWidth: 2,
-  },
-});
