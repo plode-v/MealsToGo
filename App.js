88598d86c5703934728d0fbdcc2433ecee8acf57
Plode <84858705+plode-v@users.noreply.github.com>
2024-03-21 13:23:24 +0700

    add search bar onto the main page

--- App.js
+++ App.js
@@ -1,38 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform, SafeAreaView } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+import React, { useState } from "react";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.searchContainer}>
-          <Text>search</Text>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
+            mode="view"
+          />
         </View>
         <View style={styles.mainContainer}>
           <Text>list</Text>
         </View>
-        <StatusBar style='auto' />
+        <ExpoStatusBar style="auto" />
       </SafeAreaView>
     </>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Platform.OS === "android" && 20,
+    marginTop: StatusBar.currentHeight, // only applies to Android. Same as SafeAreaView but for Android
   },
   searchContainer: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     padding: 16,
   },
   mainContainer: {
-    backgroundColor: "navy",
-    borderColor: 'black',
+    backgroundColor: "darkblue",
+    borderColor: "black",
     flex: 1,
     padding: 16,
-    borderTopColor: 'black',
-    borderTopWidth: 2
-  }
+    borderTopColor: "black",
+    borderTopWidth: 2,
+  },
 });
